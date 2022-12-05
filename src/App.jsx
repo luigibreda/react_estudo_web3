307a1d27e8f36a5a2a6492e1147816026f62264d
DanielCukier <>
2022-12-05 01:36:11 -0300

    implementando mais modificacoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
-import abi from "./utils/WavePortal.json";
-
+import abi from "./utils/WavePortal.json"; 
 
 export default function App() {
 
